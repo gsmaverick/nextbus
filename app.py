fa43b5baa2bcec597d97d67fb23e47ccd6f803b9
gsmaverick <gavin.schulz@gmail.com>
2012-11-17 00:41:48 -0500

    date fixing

--- app.py
+++ app.py
@@ -97,6 +97,12 @@ def favicon():
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
+    try:
+        os.environ['TZ'] = 'US/Eastern'
+        time.tzset()
+    except:
+        print "error"
+
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
