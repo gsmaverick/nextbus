1108640601a80056ab134b136f9917b394d97e5b
gsmaverick <gavin.schulz@gmail.com>
2012-11-18 00:37:22 -0500

    runMatch filter for jasmine tests

--- Specs/SpecHelper.js
+++ Specs/SpecHelper.js
@@ -1,3 +1,38 @@
+/**
+ * Determines if the given jasmine spec should run based on whether it's highest
+ * level suite desciption match the value of the runMatch parameter in the url.
+ *
+ * @param {Spec} spec Jasmine spec object that is about to be executed.
+ *
+ * @returns {Boolean} True if spec should run, false if it should be skipped.
+ */
+var specFilter = function(spec){
+    /**
+     * @param {String} name Key of the value to be retrieved from the url.
+     *
+     * @returns {Object} Value of the requested key or undefined if the key does
+     *     not exist in the url.
+     */
+    var getQueryParam = function(name){
+        var pairs = window.location.search.slice(1).split('&');
+
+        for (key in pairs){
+            var arr = pairs[key].split('=');
+
+            if (arr[0] === name)
+                return decodeURIComponent(arr[1].replace(/\+/g, " "));
+        }
+    }
+
+    var val = getQueryParam('runMatch') || '';
+
+    // Find the outermost suite this spec belongs to.
+    var suite = spec.suite;
+    while (suite.parentSuite) suite = suite.parentSuite;
+
+    return suite.description.toLowerCase().indexOf(val.toLowerCase()) !== -1;
+};
+
 // Makes a fake header instance that can be used to test against interactions
 // with the header.
 var makeFakeHeader = function(){
